8826d80cf76750073c11a6d7d28a3a4b16436713
Rodrigo Molina <rodrigomd94@gmail.com>
2024-05-28 12:11:38 -0600

    Remove greet

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,7 @@ import "./App.css";
 import OpenWebview from "./components/OpenWebView";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
   return (
     <div className="container">
       <h1>Welcome to Tauri!</h1>
@@ -33,22 +26,7 @@ function App() {
 
       <p>Click on the Tauri, Vite, and React logos to learn more.</p>
 
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
+      
     </div>
   );
 }
